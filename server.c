4b7f9c2f144ee7b8364994fc0763f9d1501f716f
Dominik Klon <xklond00@vutbr.cz>
2022-03-07 22:18:23 +0100

    nevim co s tim je dopici

--- server.c
+++ server.c
@@ -7,10 +7,13 @@
 
 char* readHostname();
 char* readCpuName();
-float GetCPULoad();
-struct sockaddr_in servad;
+double GetCPULoad();
 
 int main (int argc, char** argv){
+    int listenSock, connSock; long valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
 // set up the port
     if(argc != 2){
         printf("Input a correct number of a port.\n");
@@ -19,49 +22,71 @@ int main (int argc, char** argv){
 
     int port = atoi(argv[1]);
 
-    servad.sin_family = AF_INET;
-    servad.sin_addr.s_addr = htonl(INADDR_ANY);
-    servad.sin_port = htons(port);
+        address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = INADDR_ANY;
 
-    int listenSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); 
-    bind(listenSock, (struct sockaddr*) &servad , sizeof(servad));
-    listen(listenSock,10);
+     memset(address.sin_zero, '\0', sizeof address.sin_zero);
 
-    char* httpHeader = "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n";
-    GetCPULoad();
+    if ((listenSock = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("In socket");
+        exit(EXIT_FAILURE);
+    }
+    if (bind(listenSock, (struct sockaddr *)&address, sizeof(address))<0)
+    {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(listenSock, 10) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+
+    // char* httpHeader = "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n";
+    
     //hlavni prijmaci smycka
     while(1){
-        
-        char* buffer=malloc(sizeof(char)*128);
-        strcat(buffer,httpHeader);
-        int connSock = accept(listenSock,(struct sockaddr*) NULL, NULL);
-
+       // int connSock = accept(listenSock,NULL, NULL);
+        if (( connSock = accept(listenSock, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+        char buffer[256] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\n\r\n";
+       // char* buffer=malloc(sizeof(char)*128);
+       // strcat(buffer,httpHeader);
+    //printf("%s\n",buffer);
        char clientMessage[13];
        read(connSock,clientMessage,13);
         
        if(strncmp("GET /hostname",clientMessage,13)==0){
            strcat(buffer,readHostname());
-           write(connSock,buffer,strlen(buffer));  
+           send(connSock,buffer,strlen(buffer),0);  
            free(readHostname());
+
        }
        else if(strncmp("GET /cpu-name",clientMessage,13)==0){
            strcat(buffer,readCpuName());
            write(connSock,buffer,strlen(buffer));
            free(readCpuName());
+
        }
-       else if(strncmp("GET /load",clientMessage,9)==0){
-           char* buf = malloc(sizeof(char)*10);
+       else if(strncmp("GET /load",clientMessage,9)==0){ 
+          char* buf = malloc(sizeof(char)*10);
           gcvt (GetCPULoad(), 10, buf);
           strcat(buffer,buf);
           write(connSock,buffer,strlen(buffer));
-          printf("%g %% ",GetCPULoad());
           free(buf);
        }
 
-       close(connSock);
-       free(buffer);
+        memset(buffer,0,strlen(buffer));
+       
+      // free(buffer);
+      close(connSock);
     }
-    close(listenSock);
+   // close(listenSock);
     return 0;
 }
 char* readHostname(){
@@ -74,51 +99,52 @@ char* readCpuName(){
     char* cpuName = malloc(128*sizeof(char));
     FILE* f;
     f = popen("cat /proc/cpuinfo | grep \"model name\"", "r");
-    // fscanf(f,"%s",cpuName);
     fgets(cpuName,128,f);
     return cpuName;
 }
-float GetCPULoad() {
+double GetCPULoad() {
     char prevInfo[1024];
     char currInfo[1024];
-    float prevValues[10];
-    float currValues[10];
+    double prevValues[11];
+    double currValues[11];
 
     FILE* info = popen("cat /proc/stat","r");
 	fgets(prevInfo,1024,info);
+    // printf("%s\n",prevInfo);
 
     char* token = strtok(prevInfo, " ");
     for(int i=0; token != NULL; i++ ) {
-        prevValues[i] = atoi(token);  
-        //printf( "%f - ", prevValues[i] );
+        prevValues[i] = atof(token);  
+       // printf( "%f \n ", prevValues[i] );
         token = strtok(NULL, " ");
     }
-
+    
     sleep(1);
 
     info = popen("cat /proc/stat","r");
     fgets(currInfo,1024,info);
+   // printf("%s\n",currInfo);
 
-    token = strtok(currInfo, " ");
-    for(int i=0; token != NULL; i++ ) {
-        currValues[i] = atoi(token);  
-        //printf( "%f - ", currValues[i] );
-        token = strtok(NULL, " ");
+    char* token2 = strtok(currInfo, " ");
+    for(int i=0; token2 != NULL; i++ ) {
+        currValues[i] = atof(token2);  
+       // printf( "%f \n ", currValues[i] );
+        token2 = strtok(NULL, " ");
     }
+
     //this code is equivalent to https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
-    float prevIdle = prevValues[3]+prevValues[4];
-    float idle = currValues[3]+currValues[4];
-    float prevNonIdle = prevValues[0]+prevValues[1]+prevValues[2]+prevValues[5]+prevValues[6]+prevValues[7];
-    float nonIdle = currValues[0]+currValues[1]+currValues[2]+currValues[5]+currValues[6]+currValues[7];
+    double prevIdle = prevValues[4]+prevValues[5];
+    double idle = currValues[4]+currValues[5];
+    double prevNonIdle = prevValues[1]+prevValues[2]+prevValues[3]+prevValues[6]+prevValues[7]+prevValues[8];
+    double nonIdle = currValues[1]+currValues[2]+currValues[3]+currValues[6]+currValues[7]+currValues[8];
 
-    float prevTotal=prevIdle+prevNonIdle;
-    float total = idle+nonIdle;
+    double prevTotal=prevIdle+prevNonIdle;
+    double total = idle+nonIdle;
 
-    float totaf = total - prevTotal;
-    float idled = idle - prevIdle;
+    double totaf = total - prevTotal;
+    double idled = idle - prevIdle;
 
-    float CPU_Percentage = (totaf - idled)/totaf;
-    printf("%g %%",CPU_Percentage);
+    double CPU_Percentage = (totaf - idled)/totaf;
     return CPU_Percentage*100;
 }
 // int getCpuUsage(){
